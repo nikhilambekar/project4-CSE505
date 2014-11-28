eb94010cfbd4790feaba4b3c3792fb8eb30bb169
Varun Shastry <vshastry@cs.stonybrook.edu>
2014-11-27 21:44:53 -0500

    First commit
    Signed-off-by: Varun Shastry <vshastry@cs.stonybrook.edu>

---
+++ initialworld.pl
@@ -0,0 +1,44 @@
+% Initial State
+% path (Source, Destination, Cost, Weather).
+path(a, e, 5, sunny).
+path(e, d, 10, rainy).
+path(d, c, 3, sunny).
+path(b, c, 4, rainy).
+path(a, b, 2, sunny).
+path(a, d, 12, sunny).
+path(b, d, 8, sunny).
+path(c, e, 3, sunny).
+path(X, Y, Dist):- path(Y, X, Dist).
+
+% package(e, b, 		   
+bus(b,a,2).
+bus(a,d,4).
+bus(d,e,6).
+bus(e,c,2).
+
+% Variables
+taxiRate(2).
+walkLimit(15).
+
+package(a, d).
+package(a, e).
+package(b, e).
+
+% End - initial state
+
+taxi(X, Y, Cost):- path(X, Y, Dist, _), taxiRate(Rate), Cost is D * Rate.
+
+getallpathsMain(Visited, Cost):-
+	package(Start, End),
+	getallpaths(Start, End, [Start], 0, Visited, Cost).
+
+getallpaths(Start, End, InterPaths, DistAcc, Visited, TotDist):-
+	path(Start, End, Distance, _),
+	reverse([End|InterPaths], Visited),
+	TotDist is DistAcc + Distance.
+
+getallpaths(Start, End, InterPaths, DistAcc, Visited, TotDist):-
+	path(Start, InterPoint, Dist, _),
+	\+ member(InterPoint, InterPaths),
+	NewDistAcc is DistAcc + Dist,
+	getallpaths(InterPoint, End, [InterPoint|InterPaths], NewDistAcc, Visited, TotDist).
